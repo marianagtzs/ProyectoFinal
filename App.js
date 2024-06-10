6689e5294ff20b1ae2e3f76807aa11857e93cd00
seisdani <danielayalaof@gmail.com>
2024-06-09 22:15:26 -0700

    comit kjj

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native'
 import CategoriesScreen from "./screens/CategoriesScreens";
 import { StatusBar } from 'expo-status-bar';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,58 +1,52 @@
 import { View, Text, FlatList, StyleSheet } from "react-native";
-import { useLayoutEffect } from 'react'
-
+import { useLayoutEffect } from 'react';
 import { MEALS, CATEGORIES } from "../data/dummy_data";
 import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route, navigation }){
-    const catId = route.params.categoryId
-    console.log('catId', catId)
+function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
 
-    const displayedMeals = MEALS.filter((mealItem) => {
-        return mealItem.categoryIds.indexOf(catId) >= 0
-    })
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
 
-    useLayoutEffect(() => {
-        const categoryTitle = CATEGORIES.find((category) => category.id === catId).title
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
 
-        navigation.setOptions({
-            title: categoryTitle
-        })
-    }, [catId, navigation])
+    navigation.setOptions({
+      title: categoryTitle
+    });
+  }, [catId, navigation]);
 
-    function renderMealItem(itemData) {
-    
-        return(
+  function renderMealItem(itemData) {
+    return (
+      <MealItem
+        id={itemData.item.id}
+        title={itemData.item.title}
+        imageUrl={itemData.item.imageUrl}
+        affordability={itemData.item.affordability}
+        complexity={itemData.item.complexity}
+        duration={itemData.item.duration}
+      />
+    );
+  }
 
-            <MealItem
-                id={itemData.item.id} 
-                title={itemData.item.title}
-                imageUrl={itemData.item.imageUrl}
-                affordability={itemData.item.affordability}
-                complexity={itemData.item.complexity}
-                duration={itemData.item.duration}
-            />
-        )
-
-    }
-
-    return(
-        <View style={styles.container}>
-            <Text>Meals Overview - { catId }</Text>
-            <FlatList 
-                data={displayedMeals}
-                keyExtractor={(item) => item.id}
-                renderItem={renderMealItem}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
 }
 
-export default MealsOverviewScreen
+export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16
-    }
-})
+  container: {
+    flex: 1,
+    padding: 16
+  }
+});
